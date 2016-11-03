489c121bfa16233e34a9f65e01fea982b9bfb12e
Kyrylo Romanenko <kromanenko@mirantis.com>
2016-11-03 16:16:18 +0200

    Small improvement of code style

--- opti.py
+++ opti.py
@@ -1,71 +1,59 @@
 #!/usr/bin/env python
 import urllib2
 
-package_list = ['wget.tcz','python-pip.tcz','unzip.tcz','sudo.tcz','mksquashfs.tcz','gawk.tcz','genisoimage.tcz','qemu.tcz','pidgin.tcz']
+package_list = ['wget.tcz', 'python-pip.tcz', 'unzip.tcz', 'sudo.tcz',
+                'mksquashfs.tcz', 'gawk.tcz', 'genisoimage.tcz', 'qemu.tcz',
+                'pidgin.tcz']
 serv_url = "http://distro.ibiblio.org/tinycorelinux/2.x/tcz/"
 suffix = ".dep"
 UP_SET = set(package_list)
-#UP_SET.add('mupen64plus.tcz')
-DOWN_SET = set()
 deepness = 0
+
 
 def file_exists(location):
     request = urllib2.Request(location)
-    request.get_method = lambda : 'HEAD'
+    request.get_method = lambda: 'HEAD'
     try:
-        response = urllib2.urlopen(request)
+        urllib2.urlopen(request)
         return True
     except urllib2.HTTPError:
         return False
 
-def opendoor(localset):        
-    print deepness
+
+def opendoor(localset):
+    print (deepness)
     DOWN_SET_LOCAL = set()
     locallist = list(localset)
     for eat_em in locallist:
         url = serv_url + eat_em + suffix
-        if file_exists(url) == True:
+        if file_exists(url):
             file_link = urllib2.urlopen(url)
             data = file_link.read()
             deps = data.split()
             for dep in deps:
                 if dep not in UP_SET:
-#                    print dep
                     UP_SET.add(dep)
                     DOWN_SET_LOCAL.add(dep)
                     package_list.append(dep)
-                elif dep not in DOWN_SET_LOCAL :
-                    print dep,"already in UP_SET ergo deepness should be increased, now it ", deepness, " level"
+                elif dep not in DOWN_SET_LOCAL:
+                    print (dep, " already in UP_SET ergo deepness should be"
+                                "increased, now it ", deepness, " level")
                     package_list.remove(dep)
                     package_list.append(dep)
                     DOWN_SET_LOCAL.add(dep)
-                    # for index in range(len(package_list))[::-1]:
-                    #     print package_list[index]
-                    #     if package_list[index] == dep: 
-                    #         delpack = package_list.pop(index)
-                    #         package_list.append(dep)
-                    #         DOWN_SET_LOCAL.add(dep)
                 else:
-                    print "This package", dep, " already processed"
+                    print ("This package", dep, " already processed")
         else:
-            print "File not found (package", eat_em, "has no deps)"
-#    print DOWN_SET_LOCAL
+            print ("File not found (package", eat_em, "has no deps)")
+
     return DOWN_SET_LOCAL
 
 
 DOWN_SET = opendoor(UP_SET)
 while not len(DOWN_SET) == 0:
-    deepness +=1
+    deepness += 1
     DOWN_SET = opendoor(DOWN_SET)
 
-    
-
-
-
-#print opendoor(DOWN_SET)            
 for packname in UP_SET:
-    print packname
-print package_list[::-1]
-#print DOWN_SET
-#print UP_SET.difference(DOWN_SET)
-
+    print (packname)
+print (package_list[::-1])
